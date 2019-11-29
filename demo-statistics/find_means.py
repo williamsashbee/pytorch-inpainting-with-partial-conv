81402fdcd9c103c475e24a25c7c8bfdd4f19c6a5
William S Ashbee <wsashbee@gmail.com>
2019-11-29 15:24:44 -0500

    trying to shift faces

---
+++ demo-statistics/find_means.py
@@ -0,0 +1,69 @@
+from places2 import Places2
+from imutils import face_utils
+import argparse
+
+import dlib
+import cv2
+
+
+action_list = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+
+def findMeans(args, path):
+    print("[INFO] loading facial landmark predictor...")
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(args.shape_predictor)
+
+    image = cv2.imread(path)
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # detect faces in the grayscale frame
+    rects = detector(gray, 0)
+
+    # loop over the face detections
+    for rect in rects:
+        # compute the bounding box of the face and draw it on the
+        # frame
+        (bX, bY, bW, bH) = face_utils.rect_to_bb(rect)
+        cv2.rectangle(image, (bX, bY), (bX + bW, bY + bH),
+                      (0, 255, 0), 1)
+
+        # determine the facial landmarks for the face region, then
+        # convert the facial landmark (x, y)-coordinates to a NumPy
+        # array
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+
+        # loop over the (x, y)-coordinates for the facial landmarks
+        # and draw each of them
+        xyList=[]
+        for (_, (x, y)) in enumerate(shape):
+            xyList.append((x,y))
+
+        print(xyList)
+        xyList = sorted(xyList, key=lambda point: point[0])
+        print(xyList)
+        sf = .5
+        if len(xyList) == 5:
+            print("Hi")
+
+
+if __name__ == '__main__':
+    import os
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image_size', type=int, default=512)
+    parser.add_argument('--save_dir', type=str, default='mask-rect-large-temp--temp')
+    parser.add_argument('--root', type=str, default='../../../celeba-kaggle-512/data_large/train')
+    parser.add_argument("-p", "--shape-predictor",
+                    help="path to facial landmark predictor", default = "shape_predictor_5_face_landmarks.dat")
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+
+    dataset_train = Places2(args.root, None, None, None, 'demo')
+    findMeans(args, dataset_train.paths)
+
