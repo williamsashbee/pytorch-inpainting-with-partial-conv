f11d50eee9769203d8091f5d315393228bb95eaa
William S Ashbee <wsashbee@gmail.com>
2019-11-29 16:11:56 -0500

    progress on finding facial means

--- demo-statistics/find_means.py
+++ demo-statistics/find_means.py
@@ -1,52 +1,67 @@
 from places2 import Places2
 from imutils import face_utils
 import argparse
-
+import numpy as np
 import dlib
 import cv2
-
 
 action_list = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
 
-def findMeans(args, path):
+def findMeans(args, paths):
     print("[INFO] loading facial landmark predictor...")
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor(args.shape_predictor)
+    endPath = np.min([len(paths), 1000])
+    xVals = np.zeros((endPath, 5))
+    yVals = np.zeros((endPath, 5))
 
-    image = cv2.imread(path)
+    count = -1
+    for path in paths:
+        count+=1
+        if count == endPath:
+            break
+        image = cv2.imread(path)
 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # detect faces in the grayscale frame
-    rects = detector(gray, 0)
+        # detect faces in the grayscale frame
+        rects = detector(gray, 0)
 
-    # loop over the face detections
-    for rect in rects:
-        # compute the bounding box of the face and draw it on the
-        # frame
-        (bX, bY, bW, bH) = face_utils.rect_to_bb(rect)
-        cv2.rectangle(image, (bX, bY), (bX + bW, bY + bH),
+        # loop over the face detections
+        for rect in rects:
+            if len(rects) != 1:
+                break
+
+
+            # compute the bounding box of the face and draw it on the
+            # frame
+            (bX, bY, bW, bH) = face_utils.rect_to_bb(rect)
+            cv2.rectangle(image, (bX, bY), (bX + bW, bY + bH),
                       (0, 255, 0), 1)
 
-        # determine the facial landmarks for the face region, then
-        # convert the facial landmark (x, y)-coordinates to a NumPy
-        # array
-        shape = predictor(gray, rect)
-        shape = face_utils.shape_to_np(shape)
+            # determine the facial landmarks for the face region, then
+            # convert the facial landmark (x, y)-coordinates to a NumPy
+            # array
+            shape = predictor(gray, rect)
+            shape = face_utils.shape_to_np(shape)
 
-        # loop over the (x, y)-coordinates for the facial landmarks
-        # and draw each of them
-        xyList=[]
-        for (_, (x, y)) in enumerate(shape):
-            xyList.append((x,y))
+            # loop over the (x, y)-coordinates for the facial landmarks
+            # and draw each of them
+            xyList=[]
+            for (_, (x, y)) in enumerate(shape):
+                xyList.append((x,y))
 
-        print(xyList)
-        xyList = sorted(xyList, key=lambda point: point[0])
-        print(xyList)
-        sf = .5
-        if len(xyList) == 5:
-            print("Hi")
+            xyList = sorted(xyList, key=lambda point: point[0])
+
+            sf = .5
+            if len(xyList) == 5:
+                ind = 0
+                for x, y in xyList:
+                    xVals[count,ind] = x
+                    yVals[count,ind] = y
+                    ind+=1
+    print(xVals)
 
 
 if __name__ == '__main__':
@@ -55,9 +70,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--image_size', type=int, default=512)
     parser.add_argument('--save_dir', type=str, default='mask-rect-large-temp--temp')
-    parser.add_argument('--root', type=str, default='../../../celeba-kaggle-512/data_large/train')
+    parser.add_argument('--root', type=str, default='/home/washbee1/celeba-512-mix/data1024x1024-512/data_large/train')
     parser.add_argument("-p", "--shape-predictor",
-                    help="path to facial landmark predictor", default = "shape_predictor_5_face_landmarks.dat")
+                    help="path to facial landmark predictor", default = "/home/washbee1/PycharmProjects/image_inpainting/inpainting-inuse/shape_predictor_5_face_landmarks.dat")
 
     args = parser.parse_args()
 
@@ -65,5 +80,7 @@ if __name__ == '__main__':
         os.makedirs(args.save_dir)
 
     dataset_train = Places2(args.root, None, None, None, 'demo')
+    print(len(dataset_train.paths))
+
     findMeans(args, dataset_train.paths)
 
