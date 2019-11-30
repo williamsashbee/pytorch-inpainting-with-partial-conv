0ede6ef7686a7e38d241881d5a4e829090db4828
William S Ashbee <wsashbee@gmail.com>
2019-11-30 12:31:27 -0500

    places2 mods for demo

--- places2.py
+++ places2.py
@@ -20,16 +20,32 @@ class Places2(torch.utils.data.Dataset):
         else:
             self.paths = glob('{:s}/{:s}_large/*'.format(img_root, split))
 
-        self.mask_paths = glob('{:s}/*.jpg'.format(mask_root))
-        self.N_mask = len(self.mask_paths)
+        if mask_root is not None:
+            self.mask_paths = glob('{:s}/*.jpg'.format(mask_root))
+
+            self.N_mask = len(self.mask_paths)
+        else:
+            self.mask_paths = None
+            self.N_mask = None
+
+    def getIndex(self,path):
+        ind = -1
+        for p in self.paths:
+            ind+=1
+            if p == path:
+                return ind
+        return -1
+
 
     def __getitem__(self, index):
         gt_img = Image.open(self.paths[index])
         gt_img = self.img_transform(gt_img.convert('RGB'))
 
-        mask = Image.open(self.mask_paths[random.randint(0, self.N_mask - 1)])
-        mask = self.mask_transform(mask.convert('RGB'))
-        return gt_img * mask, mask, gt_img
+        if self.mask_paths is not None:
+            mask = Image.open(self.mask_paths[random.randint(0, self.N_mask - 1)])
+            mask = self.mask_transform(mask.convert('RGB'))
+            return gt_img * mask, mask, gt_img
+        return gt_img
 
     def __len__(self):
         return len(self.paths)
