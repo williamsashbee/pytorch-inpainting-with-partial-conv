6945b029ce47099c385af437b9554faa6697aabb
William S Ashbee <wsashbee@gmail.com>
2019-11-30 12:32:59 -0500

    test modifications

--- test.py
+++ test.py
@@ -8,10 +8,15 @@ from evaluation import evaluate
 from net import PConvUNet
 from util.io import load_ckpt
 
+
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"]="3"
 parser = argparse.ArgumentParser()
 # training options
-parser.add_argument('--root', type=str, default='./data')
-parser.add_argument('--snapshot', type=str, default='')
+parser.add_argument('--root', type=str, default='/home/washbee1/celeba-kaggle')
+parser.add_argument('--maskroot', type=str, default='mask')
+parser.add_argument('--snapshot', type=str, default='../inpainting-inuse/saves-data-aug-lm/ckpt/5690000.pth')
 parser.add_argument('--image_size', type=int, default=256)
 args = parser.parse_args()
 
@@ -24,7 +29,7 @@ img_transform = transforms.Compose(
 mask_transform = transforms.Compose(
     [transforms.Resize(size=size), transforms.ToTensor()])
 
-dataset_val = Places2(args.root, img_transform, mask_transform, 'val')
+dataset_val = Places2(args.root, args.maskroot, img_transform, mask_transform, 'test')
 
 model = PConvUNet().to(device)
 load_ckpt(args.snapshot, [('model', model)])
